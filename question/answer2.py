b2688ef6b15eb01a2c368c97cfbdf5af783a48f7
이민석 <dat0802@gmail.com>
2022-03-20 21:52:27 +0900

    programers folder

--- question/answer2.py
+++
@@ -1,9 +0,0 @@
-def solution(arr):
-    answer = 0
-    a = 0
-    for i in range(len(arr)):
-        a += arr[i]
-        answer = a / len(arr)
-    return answer
-
-print(solution([1, 2, 3]))
--- question/answer4,py
+++
@@ -1,6 +0,0 @@
-def solution(s, n):
-    answer = ''
-    ord('a')
-    return answer
-
-print(solution("AB", 1))
