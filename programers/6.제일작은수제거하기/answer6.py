730355d21db189fafdaca4fb8bc794930561c2d3
이민석 <dat0802@gmail.com>
2022-03-22 15:10:42 +0900

    Question6

---
+++ programers/6.제일작은수제거하기/answer6.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    if len(arr) <= 1:
+        answer.append(-1)
+        return answer
+    else:
+        arr.remove(min(arr))
+        return arr
+    
+print(solution([1,2,3,4]))
