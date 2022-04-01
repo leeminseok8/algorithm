349d71d150f91fd3c06afb515077e7733050f7e4
이민석 <dat0802@gmail.com>
2022-04-01 23:10:04 +0900

    문제 수정중

---
+++ programers/18.문자열 내림차수능로 배치하기/answer18.py
@@ -0,0 +1,16 @@
+def solution(s):
+    # answer = ''
+    a = []
+    b = 0
+    for i in s:
+        b = ord(i)
+        a.append(b)
+
+    a.sort()
+    return a.sort()
+
+# b = 'adf'
+# c = b[1]
+
+# print(c)
+print(solution('abcde'))
