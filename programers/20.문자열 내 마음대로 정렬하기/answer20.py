a5cf947ba76ff1264184bf6efcde8dae2fba1d71
이민석 <dat0802@gmail.com>
2022-04-05 16:40:57 +0900

    satisfy : Question20

--- programers/20.문자열 내 마음대로 정렬하기/answer20.py
+++ programers/20.문자열 내 마음대로 정렬하기/answer20.py
@@ -1,24 +1,18 @@
-# def solution(strings, n):
-#     answer = []
-#     for i in strings:
-        
-#     return answer
+def solution(strings, n):
+    return sorted(sorted(strings), key=lambda x: x[n])
 
-# print(solution)
+# a = [1, 2, 3, 6, 8, 7, 5]
+# c = ['adf', 'bab', 'def', 'edd', 'cqd']
+# b = sorted(c, key=lambda x: x[1])
+# print(b)
+# strings = ['daac', 'cba', 'babb', 'ecad']
+# d = sorted(strings)
+# e = sorted(d, key=lambda x: x[1])
+# print(e)
 
-a = [1, 2, 3, 6, 8, 7, 5]
-c = ['adf', 'bab', 'def', 'edd', 'cqd']
-b = sorted(c, key=lambda x: x[1])
-print(b)
-strings = ['daac', 'cba', 'babb', 'ecad']
-d = sorted(strings)
-e = sorted(d, key=lambda x: x[1])
-print(e)
+# f = 'a', 'b', 'c'
+# list(f)
+# print(f)
 
-f = 'a', 'b', 'c'
-list(f)
-print(f)
-
-g = list(map(str, a))
-
-print(g)
+# g = list(map(str, a))
+# print(g)
