ae0b520e5b591e5ce01d56c022c31a13d203f812
이민석 <dat0802@gmail.com>
2022-04-05 16:40:03 +0900

    Question20

---
+++ programers/20.문자열 내 마음대로 정렬하기/answer20.py
@@ -0,0 +1,24 @@
+# def solution(strings, n):
+#     answer = []
+#     for i in strings:
+        
+#     return answer
+
+# print(solution)
+
+a = [1, 2, 3, 6, 8, 7, 5]
+c = ['adf', 'bab', 'def', 'edd', 'cqd']
+b = sorted(c, key=lambda x: x[1])
+print(b)
+strings = ['daac', 'cba', 'babb', 'ecad']
+d = sorted(strings)
+e = sorted(d, key=lambda x: x[1])
+print(e)
+
+f = 'a', 'b', 'c'
+list(f)
+print(f)
+
+g = list(map(str, a))
+
+print(g)
