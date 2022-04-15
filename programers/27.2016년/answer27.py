9200055d872b3cb4b6b3ecf272d4635ecb112063
이민석 <dat0802@gmail.com>
2022-04-16 00:29:03 +0900

    Question27 수정

--- programers/27.2016년/answer27.py
+++ programers/27.2016년/answer27.py
@@ -1,7 +1,9 @@
 def solution(a, b):
-    m = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    y = ['SAT', 'SUN','MON','TUE','WED','THU','FRI']
-    d = sum(i for i in m[0:a])
-    p = (d + b)%7
+    m = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+    y = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    if a <= 1:
+        return y[b%7]
     
-    return y[p]
+    else:
+        d = (sum(i for i in m[0:a-1]) + b) % 7
+        return y[d]
