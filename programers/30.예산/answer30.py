d666bc0cda6432515131958918c2ff66e2c8a3f9
이민석 <dat0802@gmail.com>
2022-04-19 13:46:01 +0900

    Question30

--- programers/30.예산/answer30.py
+++ programers/30.예산/answer30.py
@@ -1,11 +1,3 @@
-# from datetime import datetime, timedelta
-
-# departure_date = "2022-10-12"
-# year, month, day = map(int,departure_date.split("-"))
-# aware_datetime = datetime(year, month, day)
-# time = aware_datetime+timedelta(seconds=6)
-# print(time)
-
 def solution(d, budget):
     s = sorted(d)
     for i in range(len(s)):
@@ -14,7 +6,3 @@ def solution(d, budget):
     return len(s)
 
 print(solution([1,1,1], 2))
-
-# s = [1,2,4,3,1]
-# i = 2
-# print(sum(s[:i]))
