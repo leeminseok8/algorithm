34b01eea9a253193f203081e2229841bf0278657
이민석 <dat0802@gmail.com>
2022-05-10 11:48:18 +0900

    Question45

---
+++ programers/level1/45.최대공약수와 최소공배수/answer45.py
@@ -0,0 +1,12 @@
+def solution(n, m):
+    a = n
+    b = m
+    if n>m:
+        n, m = m, n
+    while m%n:
+        r = m%n
+        m = n
+        n = r
+    return [n, a*b/n]
+    
+print(solution(2,5))
