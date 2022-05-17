337a4eee6c96dc20a37e1b830454368ae40a304d
이민석 <dat0802@gmail.com>
2022-05-17 12:24:25 +0900

    Kakao4

--- programers/카카오/4.키패드 누르기/answer4.py
+++ programers/카카오/4.키패드 누르기/answer4.py
@@ -1,7 +1,8 @@
 def solution(numbers, hand):
     answer = ''
-    n_line = [(x,y) for x in (1,2,3) for y in (1,2,3,4)]
-    
+    n_line = {(3*(x-1) + y) : (x,y) for x in (1,2,3,4) for y in (1,2,3)}
+    left = 10
+    right = 12
     
     for i in numbers:
         if (i == 1 or i == 4 or i == 7):
@@ -12,28 +13,36 @@ def solution(numbers, hand):
             right = i
             
         elif hand == 'left' and (i == 2 or i == 5 or i == 8):
-            if n_line[left][0]-n_line[i][0]+n_line[left][1]-n_line[i][1] <= n_line[right][0]-n_line[i][0]+n_line[right][1]-n_line[i][1]:
+            if abs(n_line[left][0]-n_line[i][0])+abs(n_line[left][1]-n_line[i][1]) <= abs(n_line[right][0]-n_line[i][0])+abs(n_line[right][1]-n_line[i][1]):
                 answer += 'L'
                 left = i
             else:
                 answer += 'R'
                 right = i
         
+        elif hand == 'left' and (i == 0):
+            if abs(n_line[left][0]-n_line[11][0])+abs(n_line[left][1]-n_line[11][1]) <= abs(n_line[right][0]-n_line[11][0])+abs(n_line[right][1]-n_line[11][1]):
+                answer += 'L'
+                left = 11
+            else:
+                answer += 'R'
+                right = 11
+        
         elif hand == 'right' and (i == 2 or i == 5 or i == 8):
-            if n_line[right][0]-n_line[i][0]+n_line[right][1]-n_line[i][1] <= n_line[left][0]-n_line[i][0]+n_line[left][1]-n_line[i][1]:
+            if abs(n_line[right][0]-n_line[i][0])+abs(n_line[right][1]-n_line[i][1]) <= abs(n_line[left][0]-n_line[i][0])+abs(n_line[left][1]-n_line[i][1]):
                 answer += 'R'
                 right = i
             else:
                 answer += 'L'
                 left = i
-                print(n_line[right][0]-n_line[i][0]+n_line[right][1]-n_line[i][1])
-                print(n_line[left][0]-n_line[i][0]+n_line[left][1]-n_line[i][1])
+
+        elif hand == 'right' and (i == 0):
+            if abs(n_line[right][0]-n_line[11][0])+abs(n_line[right][1]-n_line[11][1]) <= abs(n_line[left][0]-n_line[11][0])+abs(n_line[left][1]-n_line[11][1]):
+                answer += 'R'
+                right = 11
+            else:
+                answer += 'L'
+                left = 11
     return answer
 
-print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right'))    # "LRLLLRLLRRL"
-
-# [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
-
-
-# number_line = {(3*(x-1) + y) : (x,y) for x in (1,2,3,4) for y in (1,2,3)}
-# print(number_line)
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right'))
